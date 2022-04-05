b2ef27745c6f161d4a402c7dd8e4833ed2ddb813
oxiy <90516083+Oxiyu@users.noreply.github.com>
2022-04-05 15:08:10 +0100

    Create script.lua

---
+++ script.lua
@@ -0,0 +1,14 @@
+local url = "https://raw.githubusercontent.com/oxiyu/oxideX/main/Scripts"
+
+local games = {
+    [5602055394] = "Hood Modded";
+    [4483381587] = "Classic Baseplate";
+    [5872075530] = "Anarchy";
+}
+
+for i,v in next, games do
+    games[i] = table.concat(v:split(' '), '_')
+end
+
+local name = games[game.PlaceId] or games[game.GameId]
+return loadstring(game:HttpGet(url.. "/"..(name or "Universal")..".lua"))()

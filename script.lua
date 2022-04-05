6dc6a10e7b8422e7b82b8958387771a7d1c2b8a9
oxiy <90516083+Oxiyu@users.noreply.github.com>
2022-04-05 11:35:22 +0100

    Create script.lua

---
+++ script.lua
@@ -0,0 +1,12 @@
+--WIP
+--more games soon
+
+if game.PlaceId == "5602055394" then
+loadstring(game:HttpGet('https://raw.githubusercontent.com/Oxiyu/oxideX/main/Scripts/Hood_Modded.lua',true))()
+end 
+
+
+if game.PlaceId == "5872075530" then
+loadstring(game:HttpGet('https://raw.githubusercontent.com/Oxiyu/oxideX/main/Scripts/Anarchy.lua',true))()
+end
+
